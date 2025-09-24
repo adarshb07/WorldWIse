6282a73fbd4169d72980ba42e72330da1392dad7
adarshb07 <adarshbhardwaj285@gmail.com>
2025-09-24 13:33:32 +0530

    cities added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,63 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useState, useEffect } from "react";
+
+const BASE_URL = "http://localhost:8000";
 
 function App() {
+	const [cities, setCities] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(function () {
+		async function fetchCities() {
+			try {
+				setIsLoading(true);
+				const res = await fetch(`${BASE_URL}/cities`);
+				const data = await res.json();
+
+				setCities(data);
+			} catch (err) {
+				alert("There is an error loading data");
+			} finally {
+				setIsLoading(false);
+			}
+		}
+		fetchCities();
+	}, []);
+
 	return (
 		<BrowserRouter future={{ v7_startTransition: true }}>
 			<Routes>
-				<Route path="/" element={<HomePage />} />
+				<Route index element={<HomePage />} />
 				<Route path="product" element={<Product />} />
 				<Route path="pricing" element={<Pricing />} />
 				<Route path="login" element={<Login />} />
-				<Route path="app" element={<AppLayout />} />
+				<Route path="app" element={<AppLayout />}>
+					<Route
+						index
+						element={
+							<CityList cities={cities} isLoading={isLoading} />
+						}
+					/>
+
+					<Route
+						path="cities"
+						element={
+							<CityList cities={cities} isLoading={isLoading} />
+						}
+					/>
+					<Route
+						path="countries"
+						element={<p>List of Countries</p>}
+					/>
+					<Route path="form" element={<p>Form</p>} />
+				</Route>
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
